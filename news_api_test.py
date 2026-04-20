165ced337260edcf09445096dcaf826b0004515b
Atharva Mali <atharvamali@Atharvas-MacBook-Air.local>
2026-04-20 00:13:43 -0500

    added requests and working api script

--- news_api_test.py
+++ news_api_test.py
@@ -1,9 +1,18 @@
 import requests
+import json
 
-API_KEY = "demo_key"
+API_KEY = "26d30a9230c44ef78dcc04c737ba4346"
 
 url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}"
 
 response = requests.get(url)
+data = response.json()
 
-print(response.json())
+# Print nicely
+print(json.dumps(data, indent=2))
+
+# Save to file
+with open("news.json", "w") as f:
+    json.dump(data, f)
+
+print("Data saved to news.json")
